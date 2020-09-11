ef3b7161927e635ab51951627b654e088f42892c
Brian Goncalves <briancollog@hotmail.com>
2020-09-11 16:56:11 -0300

    Added .NET Solution / Projects

---
+++ CrossKnowledgeSolution.Services/SimpleJsonRequest.cs
@@ -0,0 +1,103 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Linq;
+using System;
+
+namespace CrossKnowledgeSolution.Services
+{
+    public class SimpleJsonRequest
+    {
+        IDistributedCache cacheService;
+        public SimpleJsonRequest(IDistributedCache cacheService)
+        {
+            this.cacheService = cacheService;
+        }
+        public async Task<string> GetAsync(string url)
+        {
+            var cacheData = await cacheService.GetStringAsync(url);
+            if (!string.IsNullOrEmpty(cacheData))
+                return cacheData;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var result = await reader.ReadToEndAsync();
+                SaveCache(url, result);
+                return result;
+            }
+        }
+
+        private void SaveCache(string key, string data)
+        {
+            var options = new DistributedCacheEntryOptions()
+                .SetSlidingExpiration(TimeSpan.FromMinutes(5))
+                .SetAbsoluteExpiration(TimeSpan.FromHours(1));
+            cacheService.SetString($"{key}", data, options);
+        }
+
+        public async Task<string> PostAsync(string url, Dictionary<string, string> data)
+        {
+            return await RequestAsync("POST", url, data);
+        }
+
+        public async Task<string> PutAsync(string url, Dictionary<string, string> data)
+        {
+            return await RequestAsync("PUT", url, data);
+        }
+
+        public async Task<string> PatchAsync(string url, Dictionary<string, string> data)
+        {
+            return await RequestAsync("PATCH", url, data);
+        }
+
+        public async Task<string> DeleteAsync(string url, Dictionary<string, string> data)
+        {
+            return await RequestAsync("DELETE", url, data);
+        }
+
+        private async Task<string> RequestAsync(string method, string url, Dictionary<string, string> data)
+        {
+            var cacheKey = GetCacheKey(method, url, data);
+            var cacheData = await cacheService.GetStringAsync(cacheKey);
+            if (!string.IsNullOrEmpty(cacheData))
+                return cacheData;
+
+            string dataString = JsonConvert.SerializeObject(data);
+            byte[] dataBytes = Encoding.UTF8.GetBytes(dataString);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            request.ContentLength = dataBytes.Length;
+            request.ContentType = "application/json";
+            request.Method = method;
+
+            using (Stream requestBody = request.GetRequestStream())
+            {
+                await requestBody.WriteAsync(dataBytes, 0, dataBytes.Length);
+            }
+
+            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                var result = await reader.ReadToEndAsync();
+                SaveCache(cacheKey, result);
+                return result;
+            }
+        }
+
+        private string GetCacheKey(string method, string url, Dictionary<string, string> data)
+        {
+            return $"{method}-{url}-{String.Join(',', data.Select(item => item.Key + "=" + item.Value))}";
+        }
+    }
+}
---
+++ WebAPI/Controllers/TestController.cs
@@ -0,0 +1,54 @@
+﻿using CrossKnowledgeSolution.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        SimpleJsonRequest requestService;
+        public TestController(IDistributedCache cacheService)
+        {
+            requestService = new SimpleJsonRequest(cacheService);
+        }
+        // GET: api/Test
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return await requestService.GetAsync("http://www.google.com");
+        }
+
+        // POST: api/Test
+        [HttpPost]
+        public async Task<string> Post([FromBody] string value)
+        {
+            var values = new Dictionary<string, string>();
+            values.Add("id", "1");
+            values.Add("value", value);
+            return await requestService.PostAsync("http://www.google.com", values);
+        }
+
+        // PUT: api/Test/5
+        [HttpPut("{id}")]
+        public async Task<string> Put(int id, [FromBody] string value)
+        {
+            var values = new Dictionary<string, string>();  
+            values.Add("id", id.ToString());
+            values.Add("value", value);
+            return await requestService.PutAsync("http://www.google.com", values);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public async Task<string> Delete(int id)
+        {
+            var values = new Dictionary<string, string>();
+            values.Add("id", id.ToString());
+            return await requestService.DeleteAsync("http://www.google.com", values);
+        }
+    }
+}
